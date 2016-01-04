c1de18699f2dbc61b6bc5395caedccfc3246dc7b
Aniket Soneji <amsoneji@gmail.com>
2016-01-03 21:32:33 -0800

    Added missing -y to octave-statistics package installation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt-add-repository ppa:octave/stable
 sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y octave
-sudo apt-get install octave-statistics
+sudo apt-get install -y octave-statistics
 sudo apt-get install -y python-pip
 sudo pip install oct2py
 echo "Vagrant bootstrap finished"
