099ddc2bf8a4ec4d66b986ed0cbef62eee0d6080
Aniket Soneji <amsoneji@gmail.com>
2016-01-03 20:55:06 -0800

    Added gitignore file and added bootstrap.sh file to help provision the vagrant VM

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,10 @@
+echo "Starting vagrant bootstrap"
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:octave/stable
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y octave
+sudo apt-get install octave-statistics
+sudo apt-get install -y python-pip
+sudo pip install oct2py
+echo "Vagrant bootstrap finished"
